aae9e627245bfa8eb648cf48522f376ec8482515
lihang.gw <lihang.gw@heyqu.net>
2021-03-04 18:41:16 +0800

    feat: 添加commitlint

--- public/js/detail.js
+++ public/js/detail.js
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2020-11-20 17:19:13
- * @LastEditTime: 2020-11-25 11:58:57
+ * @LastEditTime: 2021-03-03 15:14:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \blog_backend\public\js\detail.js
  */
 const element = document.querySelector('.content');
-element.innerHTML = element.getAttribute('innerHTML');
+element.innerHTML = window.article.content;
 
-hljs.initHighlightingOnLoad();
+window.hljs.initHighlightingOnLoad();
--- public/js/menu.js
+++ public/js/menu.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-11-27 15:58:55
- * @LastEditTime: 2020-12-14 23:16:53
+ * @LastEditTime: 2021-03-04 18:35:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \blog_backend\public\js\menu.js
@@ -11,7 +11,14 @@ const menuDOM = {
   searchInput: document.querySelector('.search-c .search'),
   searchInputC: document.querySelector('.search-c'),
   searchClear: document.querySelector('.search-c .close-icon'),
+  catalogC: document.querySelector('#catalogC'),
 };
+
+function lastItem(array) {
+  if (!Array.isArray(array)) return {};
+  if (array.length === 0) return {};
+  return array[array.length - 1];
+}
 
 function searchByKeywords(keywords) {
   window.location.href = keywords !== ''
@@ -36,8 +43,10 @@ function onWindowScroll() {
   const { y } = menuDOM.menuContainer?.getBoundingClientRect() || { y: 0 };
   if (y < -450) {
     menuDOM.searchInputC?.classList.add('float');
+    menuDOM.catalogC?.classList.add('float');
   } else {
     menuDOM.searchInputC?.classList.remove('float');
+    menuDOM.catalogC?.classList.remove('float');
   }
 }
 
@@ -50,6 +59,31 @@ function setupFlip(tick) {
   }, 1000);
 }
 
+function generateCatalogTree(contentStr) {
+  const hxElements = contentStr.match(/<h[1,2,3][^>]*>.*?<\/h[1,2,3]>/ig) || [];
+  const treeData = [];
+  hxElements.forEach((item) => {
+    const content = item.replace(/[<,h,1,2,3,>,/]/g, '');
+    const level = parseInt((item.match(/^<h([1,2,3])/) || [])[1], 10);
+    const tmp = { content, level, children: [] };
+    if (lastItem(treeData)?.level && level > lastItem(treeData)?.level) lastItem(treeData).children.push(tmp);
+    else treeData.push(tmp);
+  });
+  return treeData;
+}
+
+function walk(item) {
+  debugger;
+  const { children } = item;
+  if (children.length > 0) return `<div><ul>${item.content}</ul><ul>${item.children.map(({ content }) => `<li>${content}</li>`).join('')}</ul></div>`;
+  return `<ul>${item.content}</ul>`;
+}
+
+function generateCatalogDOM(tree) {
+  const domStr = tree.map(walk);
+  return domStr.flat().join('');
+}
+
 function initEvent() {
   menuDOM.searchInput?.addEventListener('focus', onInputFocus);
   menuDOM.searchClear?.addEventListener('click', onClearIconClick);
@@ -58,4 +92,11 @@ function initEvent() {
   setupFlip.setupFlip = setupFlip;
 }
 
+function initDOW() {
+  const treeData = generateCatalogTree(window.article.content);
+  const domStr = generateCatalogDOM(treeData);
+  console.log(domStr);
+}
+
 initEvent();
+initDOW();
