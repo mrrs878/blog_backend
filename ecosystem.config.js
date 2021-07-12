fed79bdb251311829464c36f8f644cd44650de42
mrrs878 <mrrs878@foxmail.com>
2021-07-12 21:18:18 +0800

    ci: 去除docker

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: mrrs878@foxmail.com
+ * @Date: 2021-07-12 21:08:02
+ * @LastEditors: mrrs878@foxmail.com
+ * @LastEditTime: 2021-07-12 21:14:05
+ * @FilePath: \blog_backend\ecosystem.config.js
+ */
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: '.\\dist\\src\\main',
+    },
+  ],
+};
